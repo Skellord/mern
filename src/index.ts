9e939c7985f58a88964f31183f705c95b774687f
Ruslan Galyautdinov <skell0rd@mail.ru>
2022-01-27 22:56:04 +0500

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+import mongoose from 'mongoose'
+import 'dotenv/config'
+import { throws } from 'assert'
+
+const PORT = process.env.PORT || 5000
+
+const app: Application = express()
+app.use(cors())
+
+const start = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI || '')
+        app.listen(PORT, () => console.log(`Server start on port ${PORT}`))
+    } catch (e) {
+        console.log('Server error', e)
+        process.exit(1)
+    }
+}
+app.get('/', (req: Request, res: Response) => {
+    res.send('Start server')
+})
+
+start().catch(e => throws(e))
